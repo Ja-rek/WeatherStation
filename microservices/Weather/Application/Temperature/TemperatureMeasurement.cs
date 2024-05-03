07fb19ec9a6c3d51ae010f8bbeed27c7086610b3
Ja-Rek <dto.jarek@gmail.com>
2024-05-03 16:51:21 +0200

    Add contructor for NHibernate

--- microservices/Weather/Application/Temperature/TemperatureMeasurement.cs
+++ microservices/Weather/Application/Temperature/TemperatureMeasurement.cs
@@ -2,6 +2,10 @@
 
 public class TemperatureMeasurement
 {
+    protected TemperatureMeasurement()
+    {
+    }
+
     public virtual int Id { get; init; }
     public required virtual int Value { get; set; }
     public required virtual string Quality { get; set; }
